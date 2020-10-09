d9639a9b991e9f90c5cd42d0eb36d0cca80bd756
Rene Morgado <renemorgado33@gmail.com>
2020-10-09 14:11:04 -0500

    Empezando con js

---
+++ methods.js
@@ -0,0 +1,6 @@
+const $CAT = document.getElementById('cat');
+const $NAME = document.getElementById('name');
+const $DESCRIPTION = document.getElementById('description');
+const $LENG = document.getElementById('leng');
+const $LINK = document.getElementById('link');
+const $GIF = document.getElementById('gif');
