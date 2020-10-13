0e3ac523e2148f2cb428c083a1961a652342481f
Rene Morgado <renemorgado33@gmail.com>
2020-10-12 22:07:36 -0500

    Terminado

---
+++ js/projectsRender.js
@@ -0,0 +1,45 @@
+const $CONTAINER = document.getElementById('ejercicios');
+const $API = 'https://renemorgado.herokuapp.com/api';
+var projects = [];
+var frontend = [];
+var backend = [];
+var released = [];
+var ready = false;
+
+const loadData = () => {
+    fetch($API, {
+            method: 'GET'
+        })
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(data) {
+            let e = JSON.parse(data);
+            projects = e;
+            renderProjects();
+
+        });
+}
+
+const renderProjects = () => {
+    $CONTAINER.firstElementChild.remove();
+
+    released = projects.filter(f => { return f.cat === "Released" })
+    released.forEach(p => {
+        $CONTAINER.innerHTML += proyectsLayoutR(p)
+    })
+
+    backend = projects.filter(b => { return b.cat === "Backend" })
+    backend.forEach(p => {
+        $CONTAINER.innerHTML += proyectsLayoutB(p)
+    })
+
+    frontend = projects.filter(f => { return f.cat === "Frontend" })
+    frontend.forEach(p => {
+        $CONTAINER.innerHTML += proyectsLayoutF(p)
+    })
+
+
+}
+
+loadData();
--- methods.js
+++
@@ -1,6 +0,0 @@
-const $CAT = document.getElementById('cat');
-const $NAME = document.getElementById('name');
-const $DESCRIPTION = document.getElementById('description');
-const $LENG = document.getElementById('leng');
-const $LINK = document.getElementById('link');
-const $GIF = document.getElementById('gif');
